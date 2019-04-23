9bcc770dd79e9c833442f30234eec50f3f2bd6e2
JGutierrez <jgutierrez@binarylemon.net>
2019-04-23 01:20:27 -0600

    Menú horizontal. Nuevas ideas en menú mòvil y finalización de menú de escritorio.

--- js/n-menu.js
+++ js/n-menu.js
@@ -3,12 +3,29 @@
 
 $(document).ready(function()
 {
+    // Menú horizontal (cerrar todos los menú abiertos al pulsar en otros elementos)
+    $('a.n-dropdown-menu').blur(n_dropdown_menu_close);
+    function n_dropdown_menu_close()
+    {
+        // Quitar las clases de enlaces que establecen los menús como activos (abierto)
+        $('.n-nav  a.n-dropdown-menu, .n-nav-block  a.n-dropdown-menu').removeClass('n-menu-opened');
+        // Ocultar los sub menúes.
+        $('.n-nav ul.n-dropdown-menu, .n-nav-block ul.n-dropdown-menu').hide();
+    }
+    // Menú horizontal (cerrar al presionar la tecla ESC)
+    // $(document).keyup(n_dropdown_menu_close_esc);
+    // function n_dropdown_menu_close_esc(event)
+    // {
+    //     // if (event.which == 27) {
+    //     //     n_dropdown_menu_close();
+    //     // }
+    // }
     // Menú horizontal (apertura y cierre de sub menú)
     $('a.n-dropdown-menu').click(n_dropdown_menu);
-
     function n_dropdown_menu(event)
     {
         event.preventDefault();
+        n_dropdown_menu_close();
         var submenu = $(this).attr('data-target');
         if ($(this).hasClass('n-menu-opened'))
         {
@@ -19,5 +36,12 @@ $(document).ready(function()
             $(this).addClass('n-menu-opened');
             $(submenu).show();
         }
+    }
+
+    $('.n-nav .n-nav-menu-toggle button').click(n_dropdown_mobile);
+    $('.n-nav-block .n-nav-menu-toggle button').click(n_dropdown_mobile);
+    function n_dropdown_mobile()
+    {
+        $(this).parent().parent().find('.n-nav-menu-container').toggle();
     }
 });
